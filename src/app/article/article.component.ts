16d9c9afddea1372fc9fa73e36e5bdb923d98a8e
Wasdom Kung <seken@gmx.com>
2017-09-11 16:16:33 +0100

    Support for rendering multiple articles using *ngFor eacha rticle in the array

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostBinding } from '@angular/core';
+import { Component, OnInit, Input, HostBinding } from '@angular/core';
 import { Article } from './article.model';
 
 @Component({
@@ -8,13 +8,10 @@ import { Article } from './article.model';
 })
 export class ArticleComponent implements OnInit {
   @HostBinding('attr.class') cssClass = 'row';
-  article: Article; // Use article model
+  @Input() article: Article; // Use article model
 
   constructor() {
-    this.article = new Article(
-      'Angular 2',
-      'http://angular.io',
-      10);
+  // Input populates article
 }
   // Call method in article class
   voteUp(): boolean {
