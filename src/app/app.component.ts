2b6641d48dbd88749cfb974c5fb50649f56fe6ea
Wasdom Kung <seken@gmx.com>
2017-09-11 16:27:04 +0100

    Allow users to added inputted article

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,4 +16,12 @@ export class AppComponent {
       new Article ('Angular Homepage', 'http://angular.io', 1),
     ];
   }
+  // Lets user add inputted article, adds to array
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
+    return false;
+  }
 }
